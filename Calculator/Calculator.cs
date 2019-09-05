1b56c43ae20bcfdfe228d6e233499059f3d4a133
Anonym <201710692@post.au.dk>
2019-09-05 13:24:03 +0200

    Added division and accumulator functionality to all functions. Also added sum

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -6,22 +6,55 @@ namespace Calculator
     {
         public double Add(double a, double b)
         {
-            return a + b;
+            double sum = a + b;
+            Accumulator = sum;
+            return sum;
+            
         }
 
         public double Subtract(double a, double b)
         {
-            return a - b;
+            double sum = a - b;
+            Accumulator = sum;
+            return sum;
         }
 
         public double Multiply(double a, double b)
         {
-            return a * b;
+            double sum = a * b;
+            Accumulator = sum;
+            return sum;
         }
 
         public double Power(double a, double b)
         {
-            return Math.Pow(a, b);
+            double sum =  Math.Pow(a, b);
+            Accumulator = sum;
+            return sum;
         }
+
+        public double Divide(double a, double b)
+        {
+            if (b > 0)
+            {
+                double sum = a / b;
+                Accumulator = sum;
+                return sum;
+            }
+
+            try
+            {
+                Console.WriteLine(a / b);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Can't divide by zero");
+
+            }
+
+            return 0;
+
+        }
+        public double Accumulator { get; private set; }
     }
 }
